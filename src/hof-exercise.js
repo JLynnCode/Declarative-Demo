a5ef93d831b6d83a8c6cd028842f06700f0b4ed0
gazgiz <jlynnmail@gmail.com>
2022-11-09 10:31:59 -0600

    Passing all tests, and signatures written

--- src/hof-exercise.js
+++ src/hof-exercise.js
@@ -29,24 +29,30 @@ const calculateTotalImperative = (items, tax) => {
 /**********/
 
 // prices: (items: [{price: number}]) -> [number]
-const prices = undefined; // TODO - Implementation
+// returns an array of numbers containing the values of object key 'price' in array 'items'
+const prices = (items) => items.map(item => item.price);
 
 // sum: (numbers: [number]) -> number
-const sum = undefined; // TODO - Implementation
+// returns the sum of all index values of prices
+const sum = (prices) => prices.reduce((total, value) => { return total + value; }, 0);
 
 // selectTaxable: (items: [{taxable: boolean}]) -> [{taxable: boolean}]
-const selectTaxable = undefined; // TODO - Implementation
+const selectTaxable = (items) => items.filter((item) => item["taxable"]);
 
 // applyTax: (prices: [number], tax: number) -> [number]
-const applyTax = undefined; // TODO - Implementation
+// returns an array of numbers, each representing the tax amount for each price index.
+const applyTax = (prices, tax) => prices.map(price => price *= tax);
 
-// baseSum: TODO - Type Signature
+// baseSum: (items: [{price: number, taxable: boolean}]) -> number
+// returns the sum of all prices before adding tax
 const baseSum = items => sum(prices(items));
 
-// taxSum: TODO - Type Signature
+// taxSum: (items: [{price: number, taxable: boolean}], tax: number) -> number
+// returns the sum of all additional tax to be applied
 const taxSum = (items, tax) => sum(applyTax(prices(selectTaxable(items)), tax));
 
-// calculateTotalDeclarative: TODO - Type Signature
+// calculateTotalDeclarative: (items: [{price: number, taxable: boolean}], tax: number) -> number
+// returns the sum of all taxed prices
 const calculateTotalDeclarative = (items, tax) =>
   baseSum(items) + taxSum(items, Math.abs(tax));
 
